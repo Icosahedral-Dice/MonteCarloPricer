11a26966b90e289268c7c09ba25e8f7287f6c77b
Mingsen Wang <mingsen.wang.sh@gmail.com>
2022-11-23 22:29:14 -0500

    Initial Commit

---
+++ MonteCarloPricer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MonteCarloPricer
+//
+//  Created by 王明森 on 11/23/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
